2bf02d9e892db8ec1ac30b60aa0ef54eb540bfff
Lotfi Arif <lutfi.arif2799@gmail.com>
2021-05-22 14:48:15 +0800

    Added database migrations and models

--- database/migrations/2021_05_22_013000_create_grant_access_table.php
+++ database/migrations/2021_05_22_013000_create_grant_access_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateFacultyTable extends Migration
+class CreateGrantAccessTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,12 @@ class CreateFacultyTable extends Migration
      */
     public function up()
     {
-        Schema::create('faculty', function (Blueprint $table) {
-            $table->id();
-            $table->string('');
+        Schema::create('grant_access', function (Blueprint $table) {
+            $table->id('grant_id');
+            $table->string('grant_desc');
             $table->timestamps();
+
+            $table->softDeletes();
         });
     }
 
@@ -27,6 +29,6 @@ class CreateFacultyTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('faculty');
+        Schema::dropIfExists('grant_access');
     }
 }

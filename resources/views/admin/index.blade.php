7423b03994a189905e2d7e23fac9b8a0872ecc82
Barongobber <barongobirn@gmail.com>
2021-06-02 23:18:54 +0800

    action button for DB admin
    adding button for DB admin interface

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -7,6 +7,18 @@
     <div class="tabbable">
         <div class="tab-content">
             <div class="card-body">
+                <div class="mb-3 text-right">
+                    <div class="dropdown" >
+                        <button class="btn-secondary dropdown-toggle p-1" style="border-radius: 10px;" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <i class='fas fa-stream'></i>
+                        </button>
+                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                            <a class="dropdown-item" href="#">Go to Member Side</a>
+                            <a class="dropdown-item" href="#">Go to Management Side</a>
+                            <a class="dropdown-item" href="#">Logout</a>
+                        </div>
+                    </div>
+                </div>
                 <div class="table-responsive">
                     <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
                         <thead>
